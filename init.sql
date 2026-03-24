af9e66c228248e745b3c4190f3f274d144892f41
Alexandra <alexadolph1701@gmail.com>
2026-03-24 17:41:02 +0000

    Gerði create.sql skjalið

---
+++ init.sql
@@ -0,0 +1,52 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    passwords VARCHAR(255),
+    createdAt TIMESTAMP,
+    lastLogin TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS places (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS homes (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    placeID INT NOT NULL, 
+
+    CONSTRAINT fk_place
+        FOREIGN KEY(placeID)
+        REFERENCES places(id)
+        ON DELETE RESTRICT
+)
+
+CREATE TABLE IF NOT EXISTS characters (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    homeID INT NOT NULL,
+
+    CONSTRAINT fk_home
+        FOREIGN KEY(homeID)
+        REFERENCES homes(id)
+        ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS people (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    alive BIT NOT NULL,
+    placeID INT NOT NULL,
+    characterID INT NOT NULL,
+
+    CONSTRAINT fk_place
+        FOREIGN KEY(placeID)
+        REFERENCES places(id)
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_character
+        FOREIGN KEY(characterID)
+        REFERENCES Characters(id)
+        ON DELETE RESTRICT
+);
