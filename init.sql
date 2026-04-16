309311bee2f5c04054cf20ee3e01bd0d2c19f103
Alexandra <alexadolph1701@gmail.com>
2026-04-16 12:50:13 +0000

    Setti gögn inn í init skjalið

--- init.sql
+++ init.sql
@@ -51,3 +51,54 @@ CREATE TABLE IF NOT EXISTS people (
         REFERENCES Characters(id)
         ON DELETE RESTRICT
 );
+
+TRUNCATE TABLE users RESTART IDENTITY;
+TRUNCATE TABLE places RESTART IDENTITY;
+TRUNCATE TABLE homes RESTART IDENTITY;
+TRUNCATE TABLE characters RESTART IDENTITY;
+TRUNCATE TABLE people RESTART IDENTITY;
+
+
+
+INSERT INTO places (name) 
+VALUES ('Storybrooke'), ('Enchanted Forest'), 
+    ('Neverland'), ('Wonderland'), 
+    ('Oz'), ('Underworld'), 
+    ('DunBroch'), ('Agrabah'), ('Wish Realm');
+
+INSERT INTO homes (name, placeID)
+VALUES ('Swan house', 1), ('The Mills House', 1), 
+    ('Evil Queens Palace', 2), ('The Jolly Roger', 2),
+    ('the Royal Castle', 2), ('The Dark Castle', 2), 
+    ('Tulip Pod Chamber', 2), ('A cottage', 2), ('The Forest', 2),
+    ('A cottage', 5), ('Neverland camp', 4);
+
+INSERT INTO characters (name, homeID)
+VALUES ('The Savior', 1), ('Truest beliver', 2), ('Evil Queen', 3),
+    ('Captain Hook', 4), ('Snow White', 5), ('Prince Charming', 5),
+    ('Rumplestiltskin', 6), ('Belle', 6), ('Blue Fairy', 7),
+    ('Mad Hatter', 8), ('The Huntsman', 9), ('Wicked Witch of the West', 10),
+    ('Jiminy Cricket', 9), ('Robin Hood', 9), ('Baelfire', 8), 
+    ('Red Riding Hood', 8), ('Granny', 8), ('Peter Pan', 11),
+    ('Felix', 11);
+
+INSERT INTO people (name, alive, placeID, characterID, image_url)
+VALUES ('Emma Swan', 'True', 1, 1, '', ), 
+    ('Henry Mills', 'True', 1, 2, ''),
+    ('Regina Mills', 'True', 1, 3, ''),
+    ('Killian Jones', 'True', 2, 4, ''),
+    ('Mary Margaret', 'True', 1, 5, ''), 
+    ('David Nolan', 'True', 1, 6, ''),
+    ('Mr. Gold', 'False', 1, 7, ''),
+    ('Belle', 'False', 1, 8, ''),
+    ('Reul Ghorm', 'True', 1, 9, ''),
+    ('Jefferson', 'True', 1, 10, ''),
+    ('Sheriff graham Humbert', 'False', 1, 11, ''),
+    ('Zelena', 'True', 5, 12, ''), 
+    ('Archie Hopper', 'True', 1, 13, ''),
+    ('Robin Hood', 'False', 2, 14, ''), 
+    ('Neal Cassidy', 'False', 1, 15, ''),
+    ('Ruby', 'True', 1, 16, ''),
+    ('Widow Lucas', 'True', 1, 17, ''),
+    ('Malcolm', 'False', 2, 18, ''),
+    ('Felix', 'False', 3, 19, '');
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', path.join(__dirname, 'src', 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({extended: true}))
 
